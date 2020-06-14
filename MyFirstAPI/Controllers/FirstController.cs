0abaec822dc00f2096b95c0e46c03b0b158d946f
Simon <imohsis@gmail.com>
2020-06-15 00:17:25 +0100

    Add project files.

---
+++ MyFirstAPI/Controllers/FirstController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MyFirstAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FirstController : Controller
+    {
+     
+        public string Get()
+        {
+            return "Welcome to bootcamp phase - II";
+        }
+    }
+}
